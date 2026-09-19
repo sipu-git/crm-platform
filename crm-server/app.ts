dbb354b5c0f5e06ba0f5a2415760e9efba3c4529
Sipu-982 <sipurana38@gmail.com>
2026-09-19 15:05:08 +0530

    env file modified

--- crm-server/app.ts
+++ crm-server/app.ts
@@ -43,7 +43,7 @@ export function createApp() {
   }));
   app.use(cors({
     origin: [env.clientUrl, "https://crm-platform-backend-91af.onrender.com",
-      "https://crm-platform-weld.vercel.app", "http://localhost:3000","http://192.168.1.17:3000", "https://crm-landing-application.vercel.app"],
+      "https://crm-platform-weld.vercel.app", "http://localhost:3000", "http://192.168.1.17:3000", "https://crm-landing-application.vercel.app"],
     credentials: true
   }));
   app.use(express.json({
