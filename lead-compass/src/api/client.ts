9ac82d5736014367e9b073375f2713b829a66e59
Sipu-982 <sipurana38@gmail.com>
2026-09-24 08:54:14 +0530

    project file modified

--- lead-compass/src/api/client.ts
+++ lead-compass/src/api/client.ts
@@ -1,8 +1,8 @@
 import axios, { AxiosError } from "axios";
 import { toast } from "sonner";
 
-// const BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://crm-platform-backend-91af.onrender.com/api";
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://crm-platform-backend-91af.onrender.com/api";
+// const BASE_URL = "http://localhost:5000/api";
 
 export const api = axios.create({
   baseURL: BASE_URL,
